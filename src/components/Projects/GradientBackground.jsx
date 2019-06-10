afd2223ebe808bdc5bdf7f99aeae8bf74bf3690d
Paul DiLoreto <paul.diloreto@gmail.com>
2019-06-09 22:20:26 -0700

    Changed gradient background from CSS grid back to Flexbox

--- src/components/Projects/GradientBackground.jsx
+++ src/components/Projects/GradientBackground.jsx
@@ -3,22 +3,25 @@ import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: 'grid',
-    gridTemplateRows: `1fr ${theme.spacing(16)}px 1fr`,
     position: 'absolute',
     top: 0,
     right: 0,
     bottom: 0,
     left: 0,
+    display: 'flex',
+    flexDirection: 'column',
   },
   firstColor: {
     backgroundColor: theme.palette.background.light,
+    flex: 1,
   },
   secondColor: {
     backgroundColor: theme.palette.background.dark,
+    flex: 1,
   },
   halfway: {
     background: `linear-gradient(to bottom, ${theme.palette.background.light}, ${theme.palette.background.dark})`,
+    height: theme.spacing(16),
   },
 }));
 
