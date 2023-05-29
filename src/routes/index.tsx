d526766d6fce2c72e12e150af30f96a41671038b
Paul DiLoreto <paul.diloreto@gmail.com>
2023-05-28 18:30:31 -0700

    chore(): renamed folders

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import Hero from '~/components/hero';
+import Hero from '~/components/Hero';
 
 export default component$(() => {
   return (
