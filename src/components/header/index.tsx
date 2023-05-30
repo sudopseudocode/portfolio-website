42076ae185aba7615b589101137bcbcdad4ce38c
Paul DiLoreto <paul.diloreto@gmail.com>
2023-05-29 19:55:35 -0700

    feat(): finished Header & Hero

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,37 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { $, component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 import styles from "./header.css?inline";
 
 export default component$(() => {
   useStylesScoped$(styles);
+  const mobileNavOpen = useSignal(false);
+  const closeMenu = $(() => {
+    mobileNavOpen.value = false;
+  });
+
   return (
     <header>
-      <a href="#work">work</a>
-      <a href="#about">about</a>
-      <a href="#contact">contact</a>
+      <nav class={{ "mobile-open": mobileNavOpen.value }}>
+        <a href="#work" onClick$={closeMenu}>
+          work
+        </a>
+        <a href="#about" onClick$={closeMenu}>
+          about
+        </a>
+        <a href="#contact" onClick$={closeMenu}>
+          contact
+        </a>
+      </nav>
+      <button
+        aria-label={
+          mobileNavOpen.value ? "Close navigation menu" : "Open navigation menu"
+        }
+        class="mobile-nav-button"
+        onClick$={() => (mobileNavOpen.value = !mobileNavOpen.value)}
+      >
+        <div
+          class={["hamburger-icon", { "close-icon": mobileNavOpen.value }]}
+        />
+      </button>
     </header>
   );
 });
